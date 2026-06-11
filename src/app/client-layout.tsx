90c36f90c324c086b1e480a7e52339592bb76459
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2026-06-11 17:49:25 +0300

    Updated client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,19 +1,11 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import ErrorBoundary from '@/components/ErrorBoundary'
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-  const isPPC = pathname.startsWith('/ppc')
-
-  if (isPPC) {
-    return <ErrorBoundary>{children}</ErrorBoundary>
-  }
-
   return (
     <ErrorBoundary>
       <div className="min-h-screen flex flex-col">
